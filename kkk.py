6e06bab05b18c87e7a2dae5ca9f2bf435c1e07b4
Juevisque <123718302+Juevisque@users.noreply.github.com>
2024-06-12 14:53:28 -0300

    Add files via upload

---
+++ kkk.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import speech_recognition as sr
+
+def transcrever(upload):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(upload) as source:
+        st.write("Processando...")
+        audio = recognizer.record(source)
+        texto = recognizer.recognize_google(audio, language="pt-BR")
+        st.write("Texto: ",texto)
+
+def principal():
+              st.title("Transcrição de audio: ")
+              upload = st.file_uploader("Faça upload do arquivo de audio", type=("wav"))
+              if upload is not None:
+                  transcrever(upload)
+
+principal()
+
